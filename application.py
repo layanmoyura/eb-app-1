30af6ae77aabdefaf6a311ff1841721c58442c06
Layan Moyura <84334230+layanmoyura@users.noreply.github.com>
2023-08-19 00:54:21 +0530

    first trigger

--- application.py
+++ application.py
@@ -106,7 +106,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
+    <h1>CICD pipeline</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
